58b3690f9964c6f6cb9d1c6877fa5c1ba756084b
madhura <madhurabhagwat31@gmail.com>
2022-04-19 14:19:09 +0530

    added new project

---
+++ module/Post/src/Model/Post.php
@@ -0,0 +1,49 @@
+<?php
+namespace Post\Model;
+
+class Post
+{
+    protected $id;
+    protected $title;
+    protected $description;
+    protected $category;
+
+    public function exchangeArray($data){
+        $this->id=$data['id'];
+        $this->title=$data['title'];
+        $this->description=$data['description'];
+        $this->category=$data['category'];
+
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    public function getCategory()
+    {
+        return $this->category;
+    }
+    public function getArrayCopy()
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+
+
+        ];
+    }
+
+}
---
+++ module/Post/src/Model/PostTable.php
@@ -0,0 +1,44 @@
+<?php
+namespace Post\Model;
+
+use Zend\Db\TableGateway\TableGatewayInterface;
+class PostTable
+{
+    protected $tableGateway;
+
+    function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway=$tableGateway;
+    }
+    public function fetchAll()
+    {
+        return $this->tableGateway->select();
+    }
+    public function saveData($post)
+    {
+        $data=[
+            'title'=>$post->getTitle(),
+            'description'=>$post->getDescription(),
+            'category'=>$post->getCategory(),
+
+        ];
+        if($post->getId()){
+            $this->tableGateway->update($data,['id' => $post->getId()]);
+        }
+        else
+        {
+            $this->tableGateway->insert($data);
+        }
+    }
+    public function getPost($id){
+        $data=$this->tableGateway->select([
+            'id'=>$id
+        ]);
+        return $data->current();
+    }
+    public function deletePost($id){
+        $this->tableGateway->delete([
+            'id'=>$id
+        ]);
+    }
+}
